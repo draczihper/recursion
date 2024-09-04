373547466536ff4318e0aa9d557f92a324879e11
Drac Zihper <draczihper@gmail.com>
2024-09-04 20:23:01 +0300

    Add a merge function

--- merge-sort/script.js
+++ merge-sort/script.js
@@ -1,6 +1,20 @@
 // Merge sort
 
-function mergeSort(a, b) {
-    if (a < b) return a;
-    else return mergeSort(a, b)
+function merge(arr, start, mid, end) {
+    let p = start, q = mid + 1;
+    
+    arr[end - start+1], k = 0;
+
+    for (let i = start; i <= end; i++){
+        if (p > mid) {
+            arr[k++] = arr[p++];
+        } else if (q > end) {
+            arr[k++] = arr[p++];
+        } else if (arr[k++] = arr[])
+    }
+}
+
+
+function mergeSort(arr, ) {
+    if ()
 }
